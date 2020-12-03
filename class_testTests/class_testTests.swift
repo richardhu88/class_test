8afb1d126f6c01248578dd16798bdd612320337a
richardhu88 <410621224@gms.ndhu.edu.tw>
2020-12-03 11:36:42 +0800

    Initial Commit

---
+++ class_testTests/class_testTests.swift
@@ -0,0 +1,34 @@
+//
+//  class_testTests.swift
+//  class_testTests
+//
+//  Created by NDHU_CSIE on 2020/12/3.
+//  Copyright © 2020 NDHU_CSIE. All rights reserved.
+//
+
+import XCTest
+@testable import class_test
+
+class class_testTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
